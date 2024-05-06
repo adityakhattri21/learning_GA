49d6b0537b46b1fbba7eef2e58355633baf78b4a
aditya <adityakhattri123@gmail.com>
2024-05-06 20:21:55 +0530

    wrote GA with tests

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
-const a = 5;
-const b = 10;
-console.log("Output is ------>",a+b);
+import dotenv from 'dotenv';
+import path from "path";
+import { fileURLToPath } from 'url';
+
+const __filepath = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filepath);
+console.log(__dirname,`/env.${process.env.NODE_ENV}`)
+if(process.env.NODE_ENV==='test'){
+    dotenv.config({
+        path: path.join(__dirname,`/.env.${process.env.NODE_ENV}`)
+    })
+}else{
+    dotenv.config();
+}
+
+console.log(process.env.NODE_ENV)
+const test = ()=>{
+    console.log(process.env.TEST)
+    return `This is the test env ${process.env.TEST}`
+}
+
+function sum (a,b){
+    return a+b;
+}
+
+if(process.env.NODE_ENV === 'test'){
+    test();
+}
+
+export {
+    sum,
+    test
+}
