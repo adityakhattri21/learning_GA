94fa21d92ed371faa3e26d4bdf047fa8ea452b39
aditya <adityakhattri123@gmail.com>
2024-05-07 17:28:38 +0530

    added comments

--- test.test.js
+++ test.test.js
@@ -4,6 +4,7 @@ import {app} from "./index.js"
 
 //this is the sample tests we will be using.
 //this is the new sample comment
+//checks express app now.
 describe("Sample Tests",()=>{
 
   it("should send working when / is pinged",async()=>{
